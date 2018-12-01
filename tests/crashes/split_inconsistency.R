e7675e0be260116b83e4e0ad29c59789d6fe3e88
Soeren Kuenzel <soeren.kuenzel@gmail.com>
2018-12-01 09:54:11 -0800

    Updating the split vsalidation test

--- tests/crashes/split_inconsistency.R
+++ tests/crashes/split_inconsistency.R
@@ -1,36 +1,34 @@
-# devtools::install_github("soerenkuenzel/forestry", ref = "SpecifyLinearFeatures")
-library(forestry)
-
+devtools::load_all()
 set.seed(43213)
-n <- 300
-x <- runif(n = n, min = 0, max = 2)
+n <- 100
+x <- data.frame(x = runif(n = n, min = 0, max = 2))
 y <- ifelse(x < 1, 0, 1)
-plot(x, y)
+plot(x[,1], y)
 
 forest_lin <- forestry(
-  x,
-  y,
+  x = x,
+  y = y,
   ntree = 1,
-  replace = FALSE, 
+  replace = FALSE,
   sampsize = n,
-  nodesizeStrictSpl = 101,
-  ridgeRF = TRUE, 
+  nodesizeStrictSpl = 34,
+  ridgeRF = TRUE,
   overfitPenalty = 10000
 )
 
-plot(x, predict(forest_lin, feature.new = x))
+plot(x[,1], predict(forest_lin, feature.new = x))
 
-forest_const <- forestry(
-  x,
-  y,
-  ntree = 1,
-  replace = FALSE, 
-  sampsize = 300,
-  nodesizeStrictSpl = 100,
-  ridgeRF = FALSE
-)
-
-plot(x, predict(forest_const, feature.new = x))
+# forest_const <- forestry(
+#   x,
+#   y,
+#   ntree = 1,
+#   replace = FALSE,
+#   sampsize = 300,
+#   nodesizeStrictSpl = 100,
+#   ridgeRF = FALSE
+# )
+#
+# plot(x, predict(forest_const, feature.new = x))
 
 
 
