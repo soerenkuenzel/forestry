7a43b837959cd57b947ff46ed46002f7077b5349
Rowan Cassius <rowancassius@gmail.com>
2019-03-17 22:39:10 -0700

    Created compute lp function and wrote a test

--- tests/testthat/test-compute_rf_lp.R
+++ tests/testthat/test-compute_rf_lp.R
@@ -3,10 +3,10 @@ test_that("Tests that compute the lp distances works correctly", {
   context('Test lp distances')
 
   # Set seed for reproductivity
-  set.seed(292315)
+  set.seed(292313)
 
   # Use Iris Data
-  test_idx <- sample(nrow(iris), 3)
+  test_idx <- sample(nrow(iris), 11)
   x_train <- iris[-test_idx, -1]
   y_train <- iris[-test_idx, 1]
   x_test <- iris[test_idx, -1]
@@ -15,9 +15,19 @@ test_that("Tests that compute the lp distances works correctly", {
   rf <- forestry(x = x_train, y = y_train)
   predict(rf, x_test)
 
-  # Apply lp function
-  compute_lp(object = rf, test = 1)
+  # Apply compute the l1 distances in the "Species" dimension
+  distances_1 <- compute_lp(object = rf,
+                         test = x_test,
+                         feature = "Species",
+                         p = 1)
 
+  # Apply compute the l2 distances in the "Petal.Length" dimension
+  distances_2 <- compute_lp(object = rf,
+                            test = x_test,
+                            feature = "Petal.Length",
+                            p = 2)
 
-  expect_equal(1, 1, tolerance = 1e-2)
+  # What else can we expect?
+  expect_equal(length(distances_1), nrow(x_test), tolerance = 1e-2)
+  expect_equal(length(distances_2), nrow(x_test), tolerance = 1e-2)
 })
