458085cced2a94aaf2f025461ab4e22badbef9bd
Soeren Kuenzel <soeren.kuenzel@gmail.com>
2019-03-21 16:37:14 -0700

    reviewing latest lp code

--- R/compute_rf_lp.R
+++ R/compute_rf_lp.R
@@ -1,4 +1,3 @@
-
 #' @include forestry.R
 
 # ---Computing lp distances-----------------------------------------------------
@@ -8,10 +7,9 @@
 #' @rdname compute_lp-forestry
 #' @description return lp ditances of selected test observations.
 #' @param object A `forestry` object.
-#' @param test_set A data frame of testing predictors.
+#' @param feature.new A data frame of testing predictors.
 #' @param feature A string denoting the dimension for computing lp distances.
 #' @param p A positive real number determining the norm p-norm used.
-#' @param ... additional arguments.
 #' @return A vector lp distances.
 #' @examples
 #'
@@ -33,73 +31,73 @@
 #'                           feature = "Petal.Length",
 #'                           p = 2)
 #' @export
+compute_lp <- function(object, feature.new, feature, p){
 
-
-
-compute_lp <- function(object, test_set, feature, p){
-
-  # Check the object is a "forestry" object
-  if(class(object)!="forestry"){
+  # Checks and parsing:
+  if (class(object) != "forestry") {
     stop("The object submitted is not a forestry random forest")
   }
-
-  # Ensure the test set is a data frame
-  test_set = as.data.frame(test_set)
-
-  # Extract the training data from the forest
+  feature.new <- as.data.frame(feature.new)
   train_set <- slot(object, "processed_dta")$processed_x
 
-  # Check the submitted feature is in the set of possible features
-  if(!(feature %in% colnames(train_set))){
+  if (!(feature %in% colnames(train_set))) {
     stop("The submitted feature is not in the set of possible features")
   }
 
+  # Compute distances
   y_weights <- predict(object = object,
-                       feature.new = test_set,
+                       feature.new = feature.new,
                        aggregation = "weightMatrix")$weightMatrix
 
-  if (is.factor(test_set[1, feature])) {
-
+  if (is.factor(feature.new[1, feature])) {
     # Get categorical feature mapping
-    mapping <-slot(object, "categoricalFeatureMapping")
+    mapping <- slot(object, "categoricalFeatureMapping")
 
     # Change factor values to corresponding integer levels
-    factor_vals = mapping[[1]][2][[1]]
-    map <- function(x) {return(which(factor_vals == x)[1])}
-    test_set[,feature] <- unlist(lapply(test_set[,feature], map))
+    #TODO(Rowan): This is specific for the iris data sets.
+    # * Run it on data set with several factor valued covariates
+    # * Implement a test with a data set with two factor-valued column
+    # * Try to use:
+    # processed_x <- preprocess_testing(feature.new,
+    #                                   object@categoricalFeatureCols,
+    #                                   object@categoricalFeatureMapping)
+    factor_vals <- mapping[[1]][2][[1]]
+    map <- function(x) {
+      return(which(factor_vals == x)[1])
+    }
+    feature.new[ ,feature] <- unlist(lapply(feature.new[,feature], map))
 
-    diff_mat <- matrix(test_set[,feature],
-                       nrow = nrow(test_set),
+    diff_mat <- matrix(feature.new[,feature],
+                       nrow = nrow(feature.new),
                        ncol = nrow(train_set),
                        byrow = TRUE) !=
                 matrix(train_set[,feature],
-                       nrow = nrow(test_set),
+                       nrow = nrow(feature.new),
                        ncol = nrow(train_set),
                        byrow = FALSE)
     diff_mat[diff_mat] <- 1
-  }
-  else{
-
-    diff_mat <- matrix(test_set[,feature],
-                       nrow = nrow(test_set),
+  } else {
+    diff_mat <- matrix(feature.new[,feature],
+                       nrow = nrow(feature.new),
                        ncol = nrow(train_set),
                        byrow = TRUE) -
                 matrix(train_set[,feature],
-                       nrow = nrow(test_set),
+                       nrow = nrow(feature.new),
                        ncol = nrow(train_set),
                        byrow = FALSE)
   }
 
   # Raise absoulte differences to the pth power
-  diff_mat <- abs(diff_mat)^(p)
+  diff_mat <- abs(diff_mat) ^ p
 
   # Compute final Lp distances
-  distances <- apply(y_weights * diff_mat, 1, sum)^(1/p)
+  distances <- apply(y_weights * diff_mat, 1, sum) ^ (1 / p)
 
   # Ensure that the Lp distances for a factor are between 0 and 1
-  if(is.factor(test_set[1, feature])){
-    f <- function(x){if (x>1){x = 1} else if (x<0){x = 0} else{}}
-    distances = apply(distances,f)}
+  if (is.factor(feature.new[1, feature])) {
+    distances[distances < 0] <- 0
+    distances[distances > 1] <- 1
+  }
 
   return(distances)
 }
--- tests/testthat/test-compute_rf_lp.R
+++ tests/testthat/test-compute_rf_lp.R
@@ -13,17 +13,16 @@ test_that("Tests that compute the lp distances works correctly", {
 
   # Create a random forest
   rf <- forestry(x = x_train, y = y_train)
-  predict(rf, x_test)
 
   # Compute the l1 distances in the "Species" dimension
   distances_1 <- compute_lp(object = rf,
-                            test = x_test,
+                            feature.new = x_test,
                             feature = "Species",
                             p = 1)
 
   # Compute the l2 distances in the "Petal.Length" dimension
   distances_2 <- compute_lp(object = rf,
-                            test = x_test,
+                            feature.new = x_test,
                             feature = "Petal.Length",
                             p = 2)
 
@@ -31,12 +30,14 @@ test_that("Tests that compute the lp distances works correctly", {
   expect_identical(length(distances_2), nrow(x_test))
 
   #set tolerance
-  tol = 1e-2
-  expect_gte(sum(distances_1), 0 - tol)
-  expect_lte(sum(distances_1), length(test_idx) + tol)
+  expect_equal(distances_1,
+               c(0.6757558, 0.5375544, 0.6937144, 0.6265924, 0.5884993,
+                 0.6233176, 0.5467013, 0.8047591, 0.7466187, 0.6254624,
+                 0.8397300),
+               tolerance = 1e-2)
   expect_equal(distances_2,
                c(2.628971, 2.360160, 2.177702, 2.574676, 2.404899,
                  2.212701, 2.091241, 2.622013, 2.276196, 2.465682, 2.801573),
-               tolerance = tol)
+               tolerance = 1e-2)
 })
 
