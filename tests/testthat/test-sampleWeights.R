38ab83c22e84f8da3ffd74a7756418ed864e6ecb
theo-s <theo_s@sonic.net>
2019-06-15 11:50:53 -0700

    Add test for sampleWeights

---
+++ tests/testthat/test-sampleWeights.R
@@ -0,0 +1,40 @@
+test_that("Tests if sampleWeights works", {
+  context('Tests sampleWeights')
+
+  x <- iris[, c(1,2,3)]
+  y <- iris[, 4]
+
+  x
+  y
+  iris
+
+
+  set.seed(275)
+
+  # Test forestry (mimic RF)
+  forest <- forestry(
+    x,
+    y,
+    ntree = 200,
+    replace = TRUE,
+    sample.fraction = .8,
+    mtry = 1,
+    nodesizeStrictSpl = 5,
+    nthread = 2,
+    splitrule = "variance",
+    splitratio = 1,
+    nodesizeStrictAvg = 5,
+    ridgeRF = FALSE,
+    sampleWeights = c(.1, .1, .8),
+    overfitPenalty = 50
+  )
+
+  # Test predict
+  y_pred <- predict(forest, x)
+
+  # Mean Square Error
+  sum((y_pred - y) ^ 2)
+
+  expect_equal(sum((y_pred - y) ^ 2), 3.88, tolerance = 0.5)
+
+})
